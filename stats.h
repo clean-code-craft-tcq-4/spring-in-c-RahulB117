efc3d6e1a4cb03f64f5c6aa17a6db64fe70952a8
RahulB <48803973+RahulB117@users.noreply.github.com>
2022-05-14 23:54:30 +0530

    Return type of alerters changed to int from void

--- stats.h
+++ stats.h
@@ -7,7 +7,7 @@ typedef struct
 
 Stats compute_statistics(const float* numberset, int setlength);
 
-typedef void (*alerter_funcptr)();
+typedef int (*alerter_funcptr)();
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], Stats computedStats);
 
 extern int emailAlertCallCount;
